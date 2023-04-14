f81825151482383208f70739db0d261e647143ae
ddnarvaez <dandnarvaez@misena.edu.co>
2023-04-14 17:31:40 -0500

    avance 14/04/2023

--- app/routes/backoffice.routes.js
+++ app/routes/backoffice.routes.js
@@ -3,7 +3,8 @@ import { Router } from "express";
 const router = Router();
 
 router.get("/", (req, res)=>{
-    res.render("backoffice",{name:""});
+    // res.render("backoffice",{name:""});
+    res.redirect("index")
 })
 
 export default router;
