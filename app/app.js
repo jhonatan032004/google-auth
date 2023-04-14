ff5616be6d801bdfc1b58b17df67b8c32cbe63be
ddnarvaez <dandnarvaez@misena.edu.co>
2023-04-14 17:20:08 -0500

    uso de plantillas gratis

--- app/app.js
+++ app/app.js
@@ -6,14 +6,19 @@ import "./middlewares/google.js"
 import ejs from "ejs";
 import path from "path";
 import * as url from 'url';
-import routeHome from "./routes/backofficeroutes.js";
+import routeHome from "./routes/backoffice.routes.js";
+import route from "./routes/home.routes.js";
 
 dotenv.config();
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+
 
 const app = express();
 // Middlewares
 app.use(express.json());
 app.use(passport.initialize());
+app.use(express.static(__dirname + '../public'))
 
 // Routes
 app.use("/auth",passport.authenticate("auth-google",{
@@ -25,8 +30,6 @@ app.use("/auth",passport.authenticate("auth-google",{
 }),loginRouter);
 
 
-const __filename = url.fileURLToPath(import.meta.url);
-const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 
 app.set("port", process.env.PORT || 9999);
 app.set("views", path.join(__dirname, "views"));
@@ -37,5 +40,6 @@ app.set("view engine", "ejs");
 //     res.send("HOLA BIENVENIDO");
 // });
 app.use("/", routeHome);
+app.use("/", route);
 
 export default app;
